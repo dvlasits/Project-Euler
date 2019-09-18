da6b8701a03598e5e9b1d739504ab64aebc96c12
redhell1000 <dvlasits@gmail.com>
2019-09-18 17:24:18 +0100

    updated fibonacci file

--- 67/fibonacci.py
+++ 67/fibonacci.py
@@ -4,15 +4,10 @@ from functools import lru_cache
 
 @lru_cache(maxsize = 100000)
 def fib(n):
+  global counter
+  counter += 1
   if n == 1 or n == 2:
     return 1
   return fib(n-1) + fib(n-2)
 
-counter = 1
-print(fib(1))
-while True:
-  num = fib(counter)
-  if len(str(num)) == 1000:
-    print(counter)
-    break
-  counter += 1
+print(fib(35))
