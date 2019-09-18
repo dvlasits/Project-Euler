44b73e1f0ef7fb2403d503e183dedd04d9c78f90
redhell1000 <dvlasits@gmail.com>
2019-09-18 16:39:33 +0100

    fixed 108

--- 108/108.py
+++ 108/108.py
@@ -9,16 +9,16 @@ def factors(x):
   for i in range(1, math.ceil(x**0.5)):
     if x % i == 0:
       total += 1
-  total += 1
-  return math.ceil(total /2)
+  return math.ceil(total)
 
 #for factor in factors(360):
 #    print factor
 
+
 # solve for 10
 #
 num = 2
-num = 10000
+num = 180000
 while True:
   using = num**2
   l = factors(using)
